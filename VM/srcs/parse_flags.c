72680d2ae329586ee4f041fcbaaabda06d434331
aelphias <aelphias@student.21-school.ru>
2020-12-22 16:17:24 +0300

    1]    32399 segmentation fault  ./corewar -n -dump

--- VM/srcs/parse_flags.c
+++ VM/srcs/parse_flags.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 20:51:41 by aelphias          #+#    #+#             */
-/*   Updated: 2020/12/21 20:51:41 by aelphias         ###   ########.fr       */
+/*   Updated: 2020/12/22 16:16:15 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ void check_flags(int argc, char **argv, t_flg *flg, t_plr *plr)
 		if (!(ft_strcmp(argv[i], "-dump")))
 		{	
 			argv[i + 1] ? 0 : print_error(ERR_USE);
+			if (!(argv[i + 1])) 
+				print_error(ERR_USE);
 			if (is_num(argv[i + 1]))
 				print_error(ERR_USE);
 			flg->dump = ft_atoi(argv[i + 1]);
@@ -42,9 +44,9 @@ void check_flags(int argc, char **argv, t_flg *flg, t_plr *plr)
 		}
 		if (!(ft_strcmp(argv[i], "-n")))
 		{
-			if (!(is_num(argv[i + 1])))
+			if (!(argv[i + 1])) 
 				print_error(ERR_USE);
-			if (ft_strstr(argv[i + 2], ".cor"))
+			if (!(is_num(argv[i + 1])) && ft_strstr(argv[i + 2], ".cor"))
 			{
 				flg->n = ft_atoi(argv[i + 1]);
 				plr->name = ft_strdup(argv[i + 2]);
