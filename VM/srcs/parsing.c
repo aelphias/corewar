2c8718e118cfb7700a98933878d49e924af844ed
Shonda Darron <sdarron@mi-e3.msk.21-school.ru>
2020-12-24 20:24:49 +0300

    creat_list_plr

--- VM/srcs/parsing.c
+++ VM/srcs/parsing.c
@@ -13,21 +13,35 @@
 
 #include "corewar.h"
 
-// viod read_nameint(argc, char **argv, t_plr *plr)
-// {
-// 	int i;
+void create_list_plr(t_plr *head, int val)
+{
+    t_plr * current = head;
 
-// 	i = 0;
-// 	while (i < argc)
-// 	{
-		
-// 	}
-// }
+    while (current->next != NULL) 
+        current = current->next;
+	if (!(current->next = (void*)ft_memalloc(sizeof(t_plr))))
+ 		print_error(ERR_MALLOC);
+    current->next->id = val;
+    current->next->next = NULL;
+}
+
+void print_list(t_plr *plr) 
+{
+    t_plr *current = plr;
+    while (current != NULL) 
+	{
+        printf("%d\n", current->id);
+    	current = current->next;
+	}
+}
 
 void	ft_parse(int argc, char **argv, t_flg *flg, t_plr *plr)
 {
-	//read_name(argc, argv, plr)
-	check_flags(argc, argv, flg, plr);
+	int i = 1;
+	while (i < 4)
+		create_list_plr(plr, i++);
+	print_list(plr);
+	//check_flags(argc, argv, flg, plr);
 	
 	//check_filename(argc, argv);
 	/*
