f112b6269e4cdf6e250a9eb16674311fbd60a2fe
Shonda Darron <sdarron@mi-e3.msk.21-school.ru>
2020-12-24 21:07:20 +0300

    creat_par

--- VM/srcs/parsing.c
+++ VM/srcs/parsing.c
@@ -25,20 +25,20 @@ void create_list_plr(t_plr *head, int val)
     current->next->next = NULL;
 }
 
-// void print_list(t_plr *plr) 
-// {
-//     t_plr *current = plr;
-//     while (current != NULL) 
-// 	{
-//         printf("%d\n", current->id);
-//     	current = current->next;
-// 	}
-// }
+void print_list(t_plr *plr) 
+{
+    t_plr *current = plr;
+    while (current != NULL) 
+	{
+        printf("%d\n", current->id);
+    	current = current->next;
+	}
+}
 
 void	ft_parse(int argc, char **argv, t_flg *flg, t_plr *plr)
 {
 	int i = 1;
-	while (i < 4)
+	while (i < 10)
 		create_list_plr(plr, i++);
 	print_list(plr);
 	//check_flags(argc, argv, flg, plr);
