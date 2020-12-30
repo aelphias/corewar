cb0e9cb0a8ac8aafd0a6f7624aa92fad073a517e
aelphias <aelphias@student.21-school.ru>
2020-12-30 20:45:08 +0300

    1

--- VM/srcs/fill_arena.c
+++ VM/srcs/fill_arena.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fill_arena.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kcharlet <kcharlet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 16:39:28 by kcharlet          #+#    #+#             */
-/*   Updated: 2020/12/29 18:54:16 by kcharlet         ###   ########.fr       */
+/*   Updated: 2020/12/30 14:40:54 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_copy_code(unsigned char *dst, unsigned int *src)
 	size_t i;
 
 	i = 0;
-	while (i < 48)
+	while (i < 48) // почему 48?
 	{
 		dst[i] = src[i];
 		i++;
