3488e9dad76fcdfe05236d11c989bc7db5d6c1e4
aelphias <aelphias@student.21-school.ru>
2021-01-03 19:50:20 +0300

    players order ok,  codesize fixed

--- VM/includes/corewar.h
+++ VM/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 21:13:58 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/03 14:05:05 by aelphias         ###   ########.fr       */
+/*   Updated: 2021/01/03 19:41:11 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,9 @@ void	print_list_car(t_car *car);
 */
 
 void	fill_arena(t_plr *plr, t_vm *vm, unsigned char *arena);
-void	ft_copy_code(unsigned char *dst, unsigned int *src);
+//void	ft_copy_code(unsigned char *dst, unsigned int *src);
+void	ft_copy_code(unsigned char *dst, unsigned int *src, int codesize);
+
 
 /*
 *	инициализация всего
--- VM/srcs/fill_arena.c
+++ VM/srcs/fill_arena.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   fill_arena.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kcharlet <kcharlet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 16:39:28 by kcharlet          #+#    #+#             */
-/*   Updated: 2021/01/02 20:26:26 by kcharlet         ###   ########.fr       */
+/*   Updated: 2021/01/03 19:42:52 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	ft_copy_code(unsigned char *dst, unsigned int *src)
+void	ft_copy_code(unsigned char *dst, unsigned int *src, int codesize)
 {
 	size_t i;
 
 	i = 0;
-	while (i < 48) // почему 48? обрааааати на это вниманеи возможно изза длины кода леъхи
+	while (i < codesize) // почему 48? обрааааати на это вниманеи возможно изза длины кода леъхи
 	{
 		dst[i] = src[i];
 		i++;
@@ -32,7 +32,7 @@ void	fill_arena(t_plr *plr, t_vm *vm, unsigned char *arena)
 	i = 0;
 	while (plr)
 	{
-		ft_copy_code(&(arena[plr->position]), plr->code);
+		ft_copy_code(&(arena[plr->position]), plr->code, plr->codesize);
 		plr = plr->next;
 	}
 	if (vm->dump)
@@ -40,5 +40,5 @@ void	fill_arena(t_plr *plr, t_vm *vm, unsigned char *arena)
 		dump(arena);
 		ft_printf("dump=%d\n ",vm->dump);
 	}
-	}
+}
 		
