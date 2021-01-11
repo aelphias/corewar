52363ca1ca5de64eea89d3348b51d6890d8005c7
aelphias <aelphias@student.21-school.ru>
2021-01-11 15:31:10 +0300

    bury doesnt work

--- VM/srcs/bury_car.c
+++ VM/srcs/bury_car.c
@@ -3,66 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   bury_car.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gjigglyp <gjigglyp@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 23:38:09 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/11 15:10:33 by gjigglyp         ###   ########.fr       */
+/*   Updated: 2021/01/11 15:29:57 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-/* void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
-{
-	t_list	*remove;
-	t_list	*current;
-
-	current = *begin_list;
-	while (current && current->next)
-	{
-		if ((*cmp)(current->next->data, data_ref) == 0)
-		{
-			remove = current->next;
-			current->next = current->next->next;
-			free(remove);
-		}
-		current = current->next;
-	}
-	current = *begin_list;
-	if (current && (*cmp)(current->data, data_ref) == 0)
-	{
-		*begin_list = current->next;
-		free(current);
-	}
-} */
-
 void	bury_car(t_vm *vm, t_car **head_car)
-{
-	t_car *current;
-	t_car *del;
-	
-	del = NULL;
-	current = (*head_car);
-	while (current && current->next)
-	{
-		if ((vm->cycles_to_die <= 0)
-		|| (vm->cycles - current->last_live) >= vm->cycles_to_die)
-		{
-			vm->car_count--;
-			del = current->next;
-			current->next = current->next->next;
-			free(del);
-		}
-		current = (*head_car);
-		if (current)
-		{
-			*head_car = current->next;
-			free(current);
-		}
-	}
-}
-
-/* void	bury_car(t_vm *vm, t_car **head_car)
 {
 	t_car *current;
 	t_car *del;
@@ -91,8 +41,61 @@ void	bury_car(t_vm *vm, t_car **head_car)
 				current = current->next->next;
 				free(del);
 			}
+			current = current->next;
 		}
 		else
 			current = current->next;
+	}
+}
+
+
+/* void	bury_car(t_vm *vm, t_car **head_car)
+{
+	t_car *current;
+	t_car *del;
+	
+	del = NULL;
+	current = (*head_car);
+	while (current && current->next)
+	{
+		if ((vm->cycles_to_die <= 0)
+		|| (vm->cycles - current->last_live) >= vm->cycles_to_die)
+		{
+			vm->car_count--;
+			del = current->next;
+			current->next = current->next->next;
+			free(del);
+		}
+		current = (*head_car);
+		if (current)
+		{
+			*head_car = current->next;
+			free(current);
+		}
+	}
+} */
+
+
+/* void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	t_list	*remove;
+	t_list	*current;
+
+	current = *begin_list;
+	while (current && current->next)
+	{
+		if ((*cmp)(current->next->data, data_ref) == 0)
+		{
+			remove = current->next;
+			current->next = current->next->next;
+			free(remove);
+		}
+		current = current->next;
+	}
+	current = *begin_list;
+	if (current && (*cmp)(current->data, data_ref) == 0)
+	{
+		*begin_list = current->next;
+		free(current);
 	}
 } */
