29148c3993effc50f7c7ed9086aa5009fed40ef2
Greenwheat Jigglypuff <gjigglyp@ox-p3.msk.21-school.ru>
2021-01-12 19:46:13 +0300

    op_live done?

--- VM/srcs/op_live.c
+++ VM/srcs/op_live.c
@@ -6,7 +6,7 @@
 /*   By: gjigglyp <gjigglyp@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:24:48 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/12 17:49:48 by gjigglyp         ###   ########.fr       */
+/*   Updated: 2021/01/12 19:45:27 by gjigglyp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,40 @@
 
 void	op_live(t_car *car, uint8_t *arena)
 {
+	t_vm **vm;
+	t_plr *plr;
+	
 	ft_printf("----I'm op_live():\n");
 	int arg1;
 	car->dir_size_status = car->dir_size_status;
 
-	/* if (car && arena)
+	 if (car && arena)
 	{
-		car->
+		car->last_live = (*vm)->cycles;
+		(*vm)->lived++;
+		if (plr == get_playersid((*vm)->plr_count, get_byte(arena, -1)))
+			{
+				(*vm)->winner = plr->id;
+				ft_printf("A process shows that player %d ( %s ) is alive\n",
+					plr->id, plr->name);
+			}
 	}
-	 */
+	 
 }
-/*
-t_plr		get_playersid(t_plr *plr, int plid)
+
+t_plr		*get_playersid(t_plr **plrs, uint8_t plid)
 {
 	t_plr *p;
 
 	if (plrs)
-}*/
+	{
+		p = *plrs;
+		while (p)
+		{
+			if (p->id == plid)
+				return (p);
+			p = p->next;
+		}
+	}
+	return (NULL);
+}
