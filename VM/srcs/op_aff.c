521b200128403a97db6c12b94a5be8da7cb0a912
Shonda Darron <sdarron@ox-p3.msk.21-school.ru>
2021-01-13 21:36:10 +0300

    aff02

--- VM/srcs/op_aff.c
+++ VM/srcs/op_aff.c
@@ -6,7 +6,7 @@
 /*   By: sdarron <sdarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:38:13 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/13 20:58:02 by sdarron          ###   ########.fr       */
+/*   Updated: 2021/01/13 21:18:41 by sdarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ void	op_aff(t_car *car, uint8_t *arena, t_vm *vm)
 {
 	int ch;
 
-	ch = 0;
 	if (arena && car)
 	{
-		ch = car->reg[get_byte(arena, car->pos)];
+		ch = car->reg[MINUS_ONE(get_byte(arena, car->pos))];
 		ft_printf("Aff: %c\n", (char)ch);
 		car->pos += 1;
 	}
