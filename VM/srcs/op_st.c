83f7a610883ac8401b02a78338f9b2f197301b71
Shonda Darron <sdarron@mi-a3.msk.21-school.ru>
2021-01-15 15:36:41 +0300

    op_st02

--- VM/srcs/op_st.c
+++ VM/srcs/op_st.c
@@ -6,7 +6,7 @@
 /*   By: sdarron <sdarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 22:14:14 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/15 13:56:00 by sdarron          ###   ########.fr       */
+/*   Updated: 2021/01/15 15:29:41 by sdarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@
 
 void					op_st(t_car *car, uint8_t *arena, t_vm *vm)
 {
-	int vel_in_reg1;
+	int val_in_reg1;
 	int addr_in_arg2;
 	int new_adr;
+	int arg1;
+	int arg2;
 	int shift;
 
 	new_adr = 0;
 	addr_in_arg2 = 0;
-	vel_in_reg1 = 0;
+	val_in_reg1 = 0;
 	shift = move(car);
-	vel_in_reg1 = car->reg[MINUS_ONE(update_pos(arena[car->pos + 2]))]; //?? use MINUS_ONE, exampl ch = car->reg[MINUS_ONE(get_byte(arena, car->pos))];
+	arg1 = get_arg(vm, car, 1, arena);
+	val_in_reg1 = car->reg[arg1];
 	if (car->arg_type[1] == 1)
 	{
-		car->reg[MINUS_ONE(update_pos(arena[car->pos + 3]))] = vel_in_reg1;
+		arg2 = get_arg(vm, car, 2, arena);
+		car->reg[arg2] = val_in_reg1;
 	}
 	if (car->arg_type[1] == 3)
 	{
-		addr_in_arg2 = get_arg(vm, car, car->pos + 3, arena); // ????
-		new_adr = car->pos + (addr_in_arg2 % IDX_MOD);
+		arg2 = get_arg(vm, car, 2, arena);
+		new_adr = car->pos + (arg2 % IDX_MOD);
+		// func(arena[new_adr] = val_in_reg1) 
 		// побитовая запись на карту
 	}
 	car->pos += shift;
