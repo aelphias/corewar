a176b859bd71d1eab3f8368551076cd810bc149a
Shonda Darron <sdarron@il-j5.msk.21-school.ru>
2021-01-17 14:53:03 +0300

    op_lldi

--- VM/srcs/op_lldi.c
+++ VM/srcs/op_lldi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_lldi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sdarron <sdarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:36:12 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/10 13:36:22 by aelphias         ###   ########.fr       */
+/*   Updated: 2021/01/17 14:52:15 by sdarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,37 @@
 
 void	op_lldi(t_car *car, uint8_t *arena, t_vm *vm)
 {
-	bool silence_make;
-	if (arena && car)
+	int arg1;
+	int arg2;
+	int arg3;
+	int new_adr;
+	int val;
+
+	arg1 = get_arg(vm, car, 1, arena);
+	arg2 = get_arg(vm, car, 2, arena);
+	arg3 = get_arg(vm, car, 3, arena);
+	new_adr = arg1 + arg2;
+	val = read_int(arena, new_adr, 4);
+	car->reg[arg3] = val;
+	if (car->arg_type[0] == T_REG)
+		car->pos++;
+	if (car->arg_type[0] == T_DIR)
 	{
-		silence_make = car->carry;
-		
+		if (car->dir_size_status = 4)
+			car->pos += 4;
+		if (car->dir_size_status = 2)
+			car->pos += 2;
 	}
+	if (car->arg_type[0] == T_IND)
+		car->pos += 4;
+	if (car->arg_type[1] == T_REG)
+		car->pos++;
+	if (car->arg_type[1] == T_DIR)
+	{
+		if (car->dir_size_status = 4)
+			car->pos += 4;
+		if (car->dir_size_status = 2)
+			car->pos += 2;
+	}
+	car->pos++;
 }
