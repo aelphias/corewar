650e0f7f4f004ec995bd850bfcc3baf05d41bbca
Shonda Darron <sdarron@il-j5.msk.21-school.ru>
2021-01-17 17:40:47 +0300

    zjmp.c

--- VM/srcs/op_zjmp.c
+++ VM/srcs/op_zjmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_zjmp.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gjigglyp <gjigglyp@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sdarron <sdarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:32:30 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/14 14:53:15 by gjigglyp         ###   ########.fr       */
+/*   Updated: 2021/01/17 17:35:21 by sdarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 void	op_zjmp(t_car *car, uint8_t *arena, t_vm *vm)
 {
-	int type_arg;
-	int a1;
-	int silence_gcc;
-	
-	car->pos = update_pos(car->pos);
-	type_arg = arena[update_pos(car->pos + 1)];
-	a1 = car->dir_size_status;
-	silence_gcc = car->reg[update_pos(arena[car->pos + 2])];
+	// int type_arg;
+	// int a1;
+	// int silence_gcc;
+
+	// car->pos = update_pos(car->pos);
+	// type_arg = arena[update_pos(car->pos + 1)];
+	// a1 = car->dir_size_status;
+	// silence_gcc = car->reg[update_pos(arena[car->pos + 2])];
+	int arg;
+
+	arg = get_arg(vm, car, 1, arena);
+	if (car->carry == 1)
+		car->pos = car->pos + (arg % IDX_MOD);
 }
