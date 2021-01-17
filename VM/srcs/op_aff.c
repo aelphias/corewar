528496a48b4f19d4a6114227c0aed8b8b684e512
Shonda Darron <sdarron@il-j5.msk.21-school.ru>
2021-01-17 17:11:10 +0300

    op_aff

--- VM/srcs/op_aff.c
+++ VM/srcs/op_aff.c
@@ -3,33 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   op_aff.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gjigglyp <gjigglyp@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sdarron <sdarron@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:38:13 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/14 21:20:20 by gjigglyp         ###   ########.fr       */
+/*   Updated: 2021/01/17 17:09:24 by sdarron          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-int			if_aff(int c)
-{
-	int		arg1;
-
-	arg1 = bitmask_corr(1, c);
-	if (arg1 != 1)
-		return (1);
-	return (0);
-}
-
 void	op_aff(t_car *car, uint8_t *arena, t_vm *vm)
 {
-	int ch;
+	int arg;
+	char vel;
 
 	if (arena && car)
 	{
-		ch = car->reg[MINUS_ONE(get_byte(arena, car->pos))];
-		ft_printf("Aff: %c\n", (char)ch);
+		arg = get_arg(vm, car, 1, arena);
+		vel = (char)car->reg[arg];
+		ft_printf("Aff: %c\n", vel);
 		car->pos += 1;
 	}
 }
