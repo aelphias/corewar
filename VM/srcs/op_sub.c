6f3fb8dd0a56164a19253983115547aeda2b01f8
aelphias <aelphias@student.21-school.ru>
2021-01-25 21:06:36 +0300

    fixed sub

--- VM/srcs/op_sub.c
+++ VM/srcs/op_sub.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 21:28:19 by aelphias          #+#    #+#             */
-/*   Updated: 2021/01/23 10:08:46 by aelphias         ###   ########.fr       */
+/*   Updated: 2021/01/25 21:01:56 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,16 @@ void	op_sub(t_car *car, uint8_t *arena)
 	int val;
 	
 	arg1 = get_arg(car, 1, arena);
+	car->pos = update_pos(car->pos + 1);
 	arg2 = get_arg(car, 2, arena);
+	car->pos = update_pos(car->pos + 1);
 	arg3 = get_arg(car, 3, arena);
+	car->pos = update_pos(car->pos + 1);
 	val = car->reg[arg1 - 1] - car->reg[arg2 - 1];//  TO_DO обращение к reg через MINUS_ONE
 	car->reg[arg3 - 1] = val;
 	if (val)
 		car->carry = 1;
 	else
 		car->carry = 0;
-	car->pos += 3;
 	car->pc = car->pos;
 }
