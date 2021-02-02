47b116d1110f0bf2f5c9ab66582d8b03e2dac481
aelphias <aelphias@student.21-school.ru>
2021-02-02 20:07:37 +0300

    1

--- VM/srcs/dump.c
+++ VM/srcs/dump.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 18:46:26 by kcharlet          #+#    #+#             */
-/*   Updated: 2021/02/02 18:48:38 by aelphias         ###   ########.fr       */
+/*   Updated: 2021/02/02 20:01:45 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	dump(uint8_t *arena)
 	while (++i <= MEM_SIZE)
 	{
 		ft_printf("%02x ", arena[i - 1]);
-		if (i % 64 == 0 && i != 0)
+		if (i % 32 == 0 && i != 0)
 		{
 			ft_printf("\n");
 			(i < MEM_SIZE) ? ft_printf("0x%04x : ", i) : 0;
